0e7b6ab915a3b7cb2af1b8280173de013e411eee
maxki <max.kiyanko@gmail.com>
2026-04-07 19:35:53 +0300

    Changing the project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [like, setLike] = React.useState(0)
+    const incrementLike = () => {setLike(like + 1)}
+    const decrementLike = () => {setLike(like - 1)}
+    return (
+        <div className="App">
+            <h1>{like}</h1>
+            <button onClick={incrementLike}>Повышение</button>
+            <button onClick={decrementLike}>Понижение</button>
+        </div>
+    );
 }
 
 export default App;
