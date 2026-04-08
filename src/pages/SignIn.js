1b5ba7cb70c8224a1c943b98a1ac75938914f342
maxki <max.kiyanko@gmail.com>
2026-04-08 21:54:39 +0300

    Login page works

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,8 +1,68 @@
-import React from 'react';
+import {useState} from 'react';
+import {useNavigate, Link} from 'react-router-dom';
+import {useAuth} from '../contexts/AuthContext';
+
 
 const SignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const {login} = useAuth();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setError('');
+
+        try {
+            const response = await fetch('http://localhost:8080/api/auth/sign-in', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({email: email, password: password}),
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                setError(data.message || 'Ошибка входа!');
+            }
+
+            login(data.token);
+            navigate('/');
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
     return (
-    <h1>Страница входа</h1>
+        <div>
+            <h2>Вход</h2>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label> Имя пользователя или email: </label>
+                    <input
+                        type="text"
+                        value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                    />
+                </div>
+                <div>
+                    <label> Пароль:</label>
+                    <input
+                    type="text"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                    />
+                </div>
+                    <button type="submit">Войти</button>
+            </form>
+            <p>
+                Нет аккаунта?! Ну и пошёл нахуй отсюда <Link to="/signup">Зарегистрироваться</Link>
+            </p>
+        </div>
     );
 };
 
